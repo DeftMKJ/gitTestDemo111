f693d7662d7626ed662ac95c0ac3b367ece1771d
Deft_MKJing <mkjing@MKJINGdeMac-mini.local>
2016-12-01 18:34:06 +0800

    111

---
+++ 111111111/111111111/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  111111111
+//
+//  Created by MKJING on 16/12/1.
+//  Copyright © 2016年 MKJING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 111111111/111111111/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  111111111
+//
+//  Created by MKJING on 16/12/1.
+//  Copyright © 2016年 MKJING. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
